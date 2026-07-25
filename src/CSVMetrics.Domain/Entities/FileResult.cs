70f90b89b9f801133590efa01784979011bac7ab
Noisy_Bomb <vladimirylanov98@gmail.com>
2026-07-26 02:35:43 +0300

    Added FileResult.cs and MeasurementValue.cs which describe values and csv files

---
+++ src/CSVMetrics.Domain/Entities/FileResult.cs
@@ -0,0 +1,15 @@
+﻿namespace CSVMetrics.Domain.Entities;
+
+public class FileResult
+{
+    public long Id { get; set; }
+    public string FileName { get; set; } = default!;
+    public double TimeDeltaSeconds { get; set; }
+    public DateTimeOffset StartDate { get; set; }
+    public double AvgExecutionTime { get; set; }
+    public double AvgValue { get; set; }
+    public double MedianValue { get; set; }
+    public double MaxValue { get; set; }
+    public double MinValue { get; set; }
+    public DateTimeOffset ProcessedAt { get; set; }
+}
---
+++ src/CSVMetrics.Domain/Entities/MeasurementValue.cs
@@ -0,0 +1,10 @@
+﻿namespace CSVMetrics.Domain.Entities;
+
+public class MeasurementValue
+{
+    public long Id { get; set; }
+    public string FileName { get; set; } = default!;
+    public DateTimeOffset Date { get; set; }
+    public double ExecutionTime { get; set; }
+    public double Value { get; set; }
+}
