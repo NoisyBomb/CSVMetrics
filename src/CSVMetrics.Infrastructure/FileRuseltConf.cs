7658e2e2c5b90f5bb539434d0bd3eb0576198957
Noisy_Bomb <vladimirylanov98@gmail.com>
2026-07-26 20:40:01 +0300

    Done

--- src/CSVMetrics.Infrastructure/FileRuseltConf.cs
+++ src/CSVMetrics.Infrastructure/FileRuseltConf.cs
@@ -4,7 +4,16 @@ using CSVMetrics.Domain.Entities;
 
 namespace CSVMetrics.Infrastructure;
 
-public class FileRuseltConf
+public class FileResultConf : IEntityTypeConfiguration<FileResult>
 {
-    
+    public void Configure(EntityTypeBuilder<FileResult> builder)
+    {
+        builder.ToTable("Results");
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.FileName).IsRequired().HasMaxLength(260);
+        builder.HasIndex(x => x.FileName).IsUnique();
+        builder.HasIndex(x => x.StartDate);
+        builder.HasIndex(x => x.AvgValue);
+        builder.HasIndex(x => x.AvgExecutionTime);
+    }
 }
