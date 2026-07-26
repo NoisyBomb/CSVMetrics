40acc5ae999c3cab18da489d3a5e8415581c9a9c
Noisy_Bomb <vladimirylanov98@gmail.com>
2026-07-26 19:04:59 +0300

    Completed MeasurementValueConf.cs with filtering indexes

--- src/CSVMetrics.Infrastructure/MeasurementValueConf.cs
+++ src/CSVMetrics.Infrastructure/MeasurementValueConf.cs
@@ -4,7 +4,13 @@ using CSVMetrics.Domain.Entities;
 
 namespace CSVMetrics.Infrastructure;
 
-public class MeasurementValueConf
+public class MeasurementValueConfiguration : IEntityTypeConfiguration<MeasurementValue>
 {
-    
+    public void Configure(EntityTypeBuilder<MeasurementValue> builder)
+    {
+        builder.ToTable("Values");
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.FileName).IsRequired().HasMaxLength(260); //Win max path length
+        builder.HasIndex(x => new { x.FileName, x.Date }); //Kapec cool tema, srazu sorted
+    }
 }
