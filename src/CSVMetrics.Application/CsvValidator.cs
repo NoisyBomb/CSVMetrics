bd8f254fc468e07ffc6b0b8c09a34e5cfbd05e87
Noisy_Bomb <vladimirylanov98@gmail.com>
2026-07-27 22:52:54 +0300

    Validator added

---
+++ src/CSVMetrics.Application/CsvValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace CSVMetrics.Application;
+
+public class CsvValidator : AbstractValidator<CsvDto>
+{
+    public CsvValidator()
+    {
+        RuleFor(x => x.Date).LessThanOrEqualTo(DateTimeOffset.UtcNow).GreaterThanOrEqualTo(new DateTimeOffset(2000, 1, 1, 0, 0, 0, TimeSpan.Zero));
+        RuleFor(x => x.ExecutionTime).GreaterThanOrEqualTo(0);
+        RuleFor(x => x.Value).GreaterThanOrEqualTo(0);
+    }
+}
