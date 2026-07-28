9095b1804293cec06c3c41892cbb5f41f57dab1f
Noisy_Bomb <vladimirylanov98@gmail.com>
2026-07-28 16:42:43 +0300

    FileResultRepository.cs fully functional

---
+++ src/CSVMetrics.Infrastructure/FileResultRepository.cs
@@ -0,0 +1,27 @@
+﻿using CSVMetrics.Application;
+using CSVMetrics.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CSVMetrics.Infrastructure;
+
+public class FileResultRepository : IFileResultRepository
+{
+    private readonly AppDb _context;
+    public FileResultRepository(AppDb context)
+    {
+        _context = context;
+    }
+    public async Task<FileResult?> GetByFileNameAsync(string fileName)
+    {
+        return await _context.Results.FirstOrDefaultAsync(r => r.FileName == fileName);
+    }
+    public async Task AddAsync(FileResult result)
+    {
+        await _context.Results.AddAsync(result);
+    }
+    public async Task DeleteAsync(FileResult result)
+    {
+        _context.Results.Remove(result);
+        await Task.CompletedTask;
+    }
+}
