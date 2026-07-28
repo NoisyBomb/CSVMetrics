515cb89c185d0f4d342c13339ae1ae1ee2e2fe11
Noisy_Bomb <vladimirylanov98@gmail.com>
2026-07-28 17:03:10 +0300

    3rd class functional

---
+++ src/CSVMetrics.Infrastructure/Transaction.cs
@@ -0,0 +1,30 @@
+﻿using CSVMetrics.Application;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace CSVMetrics.Infrastructure;
+
+public class Transaction : ITransaction
+{
+    private readonly AppDb _context;
+    private IDbContextTransaction? _transaction;
+    public Transaction(AppDb context)
+    {
+        _context = context;
+    }
+    public async Task<IDisposable> BeginTransactionAsync()
+    {
+        _transaction = await _context.Database.BeginTransactionAsync();
+        return _transaction;
+    }
+    public async Task SaveChangesAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+    public async Task CommitAsync()
+    {
+        if (_transaction != null)
+        {
+            await _transaction.CommitAsync();
+        }
+    }
+}
